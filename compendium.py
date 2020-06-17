6d276248d3ba4f7d0dcb3156ba9840fc63c1f826
Randy C. Will <randall.will@gmail.com>
2020-06-16 19:46:26 -0700

    s/python+gtk/wxpython/

---
+++ compendium.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+import wx
+import wx.aui
+
+class TabPanel(wx.Panel):
+    """
+    This will be the first notebook tab
+    """
+    def __init__(self, parent):
+        """"""
+
+        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        txtOne = wx.TextCtrl(self, wx.ID_ANY, "")
+        txtTwo = wx.TextCtrl(self, wx.ID_ANY, "")
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(txtOne, 0, wx.ALL, 5)
+        sizer.Add(txtTwo, 0, wx.ALL, 5)
+
+        self.SetSizer(sizer)
+
+
+class DemoPanel(wx.Panel):
+    """
+    This will be the first notebook tab
+    """
+    #----------------------------------------------------------------------
+    def __init__(self, parent):
+        """"""
+        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
+
+        # create the AuiNotebook instance
+        self.nb = wx.aui.AuiNotebook(self)
+
+        # add some pages to the notebook
+        pages = [(TabPanel(self.nb), "Tab 1"),
+                 (TabPanel(self.nb), "Tab 2"),
+                 (TabPanel(self.nb), "Tab 3")]
+        for page, label in pages:
+            self.nb.AddPage(page, label)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.nb, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+        self.Bind(wx.aui.EVT_AUINOTEBOOK_PAGE_CHANGED, self.OnPageChanged)
+        self.Bind(wx.aui.EVT_AUINOTEBOOK_PAGE_CHANGING, self.OnPageChanging)
+
+    def OnPageChanged(self, event):
+        old = event.GetOldSelection()
+        new = event.GetSelection()
+        sel = self.nb.GetSelection()
+        print('OnPageChanged,  old:%d, new:%d, sel:%d' % (old, new, sel))
+        event.Skip()
+
+    def OnPageChanging(self, event):
+        old = event.GetOldSelection()
+        new = event.GetSelection()
+        sel = self.nb.GetSelection()
+        print('OnPageChanging, old:%d, new:%d, sel:%d' % (old, new, sel))
+        event.Skip()
+
+
+class MainWindow(wx.Frame):
+    def __init__(self, filename='noname.txt'):
+        super(MainWindow, self).__init__(None, size=(-1,-1))
+        self.filename = filename
+        self.dirname = '.'
+        self.CreateInteriorWindowComponents()
+        self.CreateExteriorWindowComponents()
+
+    def CreateInteriorWindowComponents(self):
+        ''' Create "interior" window components. In this case it is just a
+            simple multiline text control. '''
+        DemoPanel(self)
+#        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+
+    def CreateExteriorWindowComponents(self):
+        ''' Create "exterior" window components, such as menu and status
+            bar. '''
+        self.CreateMenu()
+        self.CreateStatusBar()
+        self.SetTitle()
+
+    def CreateMenu(self):
+        fileMenu = wx.Menu()
+        for id, label, helpText, handler in \
+            [(wx.ID_ABOUT, '&About', 'Information about this program',
+                self.OnAbout),
+             (wx.ID_OPEN, '&Open', 'Open a new file', self.OnOpen),
+             (wx.ID_SAVE, '&Save', 'Save the current file', self.OnSave),
+             (wx.ID_SAVEAS, 'Save &As', 'Save the file under a different name',
+                self.OnSaveAs),
+             (None, None, None, None),
+             (wx.ID_EXIT, 'E&xit', 'Terminate the program', self.OnExit)]:
+            if id == None:
+                fileMenu.AppendSeparator()
+            else:
+                item = fileMenu.Append(id, label, helpText)
+                self.Bind(wx.EVT_MENU, handler, item)
+
+        menuBar = wx.MenuBar()
+        menuBar.Append(fileMenu, '&File') # Add the fileMenu to the MenuBar
+        self.SetMenuBar(menuBar)  # Add the menuBar to the Frame
+
+    def SetTitle(self):
+        # MainWindow.SetTitle overrides wx.Frame.SetTitle, so we have to
+        # call it using super:
+        super(MainWindow, self).SetTitle('Editor %s'%self.filename)
+
+
+    # Helper methods:
+
+    def defaultFileDialogOptions(self):
+        ''' Return a dictionary with file dialog options that can be
+            used in both the save file dialog as well as in the open
+            file dialog. '''
+        return dict(message='Choose a file', defaultDir=self.dirname,
+                    wildcard='*.*')
+
+    def askUserForFilename(self, **dialogOptions):
+        dialog = wx.FileDialog(self, **dialogOptions)
+        if dialog.ShowModal() == wx.ID_OK:
+            userProvidedFilename = True
+            self.filename = dialog.GetFilename()
+            self.dirname = dialog.GetDirectory()
+            self.SetTitle() # Update the window title with the new filename
+        else:
+            userProvidedFilename = False
+        dialog.Destroy()
+        return userProvidedFilename
+
+    # Event handlers:
+
+    def OnAbout(self, event):
+        dialog = wx.MessageDialog(self, 'A sample editor\n'
+            'in wxPython', 'About Sample Editor', wx.OK)
+        dialog.ShowModal()
+        dialog.Destroy()
+
+    def OnExit(self, event):
+        self.Close()  # Close the main window.
+
+    def OnSave(self, event):
+        textfile = open(os.path.join(self.dirname, self.filename), 'w')
+        textfile.write(self.control.GetValue())
+        textfile.close()
+
+    def OnOpen(self, event):
+        if self.askUserForFilename(style=wx.OPEN,
+                                   **self.defaultFileDialogOptions()):
+            textfile = open(os.path.join(self.dirname, self.filename), 'r')
+            self.control.SetValue(textfile.read())
+            textfile.close()
+
+    def OnSaveAs(self, event):
+        if self.askUserForFilename(defaultFile=self.filename, style=wx.SAVE,
+                                   **self.defaultFileDialogOptions()):
+            self.OnSave(event)
+
+
+app = wx.App()
+frame = MainWindow()
+frame.Show()
+app.MainLoop()
